1ccf2aee7a87375bb6fd464caadfa871ccddc82d
gwangryeol-edu <rhkr0427rhkr@gmail.com>
2025-12-19 09:17:43 +0900

    first

---
+++ src/main/java/com/example/retripbackend/RetripBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.retripbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetripBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetripBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/retripbackend/RetripBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retripbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetripBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
